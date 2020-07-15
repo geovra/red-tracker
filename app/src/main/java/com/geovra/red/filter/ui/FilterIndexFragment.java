947ca0e38c69c1b0b7f7f1af7a9245e7a4774d3f
George Vranceanu <g_vranceanu@yahoo.com>
2020-07-15 09:44:39 +0300

    44a75dc64b1120e4d4a9c3c080b4edf86b5f474856d6a3

--- app/src/main/java/com/geovra/red/filter/ui/FilterIndexFragment.java
+++ app/src/main/java/com/geovra/red/filter/ui/FilterIndexFragment.java
@@ -2,6 +2,7 @@ package com.geovra.red.filter.ui;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
