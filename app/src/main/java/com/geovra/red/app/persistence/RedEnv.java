a3927a53f83629066331ef08eea2966dd938d136
George Vrancianu <gvr@smartpay-software.ro>
2020-12-09 15:53:27 +0200

    5ee76e11774cf4c848561cd532c4dea904b54d759b

--- app/src/main/java/com/geovra/red/app/persistence/RedEnv.java
+++ app/src/main/java/com/geovra/red/app/persistence/RedEnv.java
@@ -1,4 +1,37 @@
 package com.geovra.red.app.persistence;
 
-class RedEnv {
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class RedEnv {
+
+  /**
+   * Read environment properties
+   *
+   * @return Properties
+   */
+  public static Properties init(AppCompatActivity target) {
+    Resources resources = target.getResources();
+    AssetManager assetManager = resources.getAssets();
+
+    try {
+      Properties properties = new Properties();
+      properties.load(assetManager.open("app.properties"));
+
+      // Write properties to SharedPreferences for global access
+      // TODO RedPrefs.putString(...);
+
+      return properties;
+    } catch (Exception e) {
+      System.err.println("Failed to open properties file");
+      e.printStackTrace();
+    }
+
+    return null;
+  }
 }
